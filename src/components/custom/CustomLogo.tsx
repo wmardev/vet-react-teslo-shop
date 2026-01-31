8d79987336035e72daca2f252b1dce69ca220e16
wmartinez <wmartinez@wmartinez.com>
2026-01-31 15:20:53 -0300

    correccion logo

--- src/components/custom/CustomLogo.tsx
+++ src/components/custom/CustomLogo.tsx
@@ -4,11 +4,11 @@ interface Props {
   subtitle?: string;
 }
 
-export const CustomLogo = ({ subtitle = 'Shop' }: Props) => {
+export const CustomLogo = ({ subtitle = 'Veterinaria' }: Props) => {
   return (
     <Link to="/" className="flex items-center whitespace-nowrap">
       <span className="font-montserrat font-bold text-xl m-0 whitespace-nowrap">
-        Teslo |
+        La Victoria |
       </span>
       <p className="text-muted-foreground m-0 px-2 whitespace-nowrap">
         {subtitle}
